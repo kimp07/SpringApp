c774001977904bc0089ec8590ccca00f37da9263
zamdirit <zamdirit@zamit.generalfresh.local>
2020-12-14 12:30:51 +0200

    Thymeleaf Started

--- src/main/java/org/alex/springapp/controller/MainWebController.java
+++ src/main/java/org/alex/springapp/controller/MainWebController.java
@@ -2,6 +2,7 @@ package org.alex.springapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,6 +17,15 @@ public class MainWebController {
     public String indexPage() {
         return "/index";
     }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/login";
+    }
     
+    @PostMapping("/loginAction")
+    public String loginAction() {
+        return "redirect:/web/index";
+    }
     
 }
